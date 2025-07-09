8179f1908ae61079f382b34317ec607e57a5417e
ismail varlı <ismailvarli1907@gmail.com>
2025-07-09 17:53:24 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+using HadımkoyAnkaraNakliyat_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HadımkoyAnkaraNakliyat_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkımızda()
+        {
+            return View();
+        }
+
+        public IActionResult Hizmetlerimiz()
+        {
+            return View();
+        }
+        public IActionResult Hadimköy_Ankara_Nakliyat()
+        {
+            return View();
+        }
+        public IActionResult İstanbul_Ankara_Nakliyat()
+        {
+            return View();
+        }
+        public IActionResult İstanbul_Ankara_Kamyonet_Nakliyat()
+        {
+            return View();
+        }
+        public IActionResult İstanbul_Ankara_Ambar_Nakliyat()
+        {
+            return View();
+        }
+        public IActionResult İstanbul_Ankara_Parsiyel_Nakliyat()
+        {
+            return View();
+        }
+        public IActionResult İstanbul_Ankara_Şehirlerarasi_Nakliyat()
+        {
+            return View();
+        }
+        public IActionResult()
+        {
+            return View();
+        }
+        public IActionResult()
+        {
+            return View();
+        }
+        public IActionResult İletisim()
+        {
+            return View();
+        }
+
+
+
+    }
+}
