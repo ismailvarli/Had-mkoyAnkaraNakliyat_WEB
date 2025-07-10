ee46ef7a0a8afe0b8a87e10ee55d79375e073b9c
ismail varlı <ismailvarli1907@gmail.com>
2025-07-11 01:52:02 +0300

    Hadımköy Ankara Nakliyat

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,19 @@
-using System.Diagnostics;
 using HadımkoyAnkaraNakliyat_WEB.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
 
 namespace HadımkoyAnkaraNakliyat_WEB.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
         public IActionResult Index()
         {
             return View();
@@ -44,20 +52,60 @@ namespace HadımkoyAnkaraNakliyat_WEB.Controllers
         {
             return View();
         }
-        public IActionResult()
+        public IActionResult İstanbul_Ankara_Evden_Eve_Nakliyat()
         {
             return View();
         }
-        public IActionResult()
+        public IActionResult İstanbul_Ankara_Ceyiz_Nakliyat()
         {
             return View();
         }
+        public IActionResult İstanbul_Ankara_Fuar_Nakliyat()
+        {
+            return View();
+        }
+        [HttpGet]
         public IActionResult İletisim()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult İletisim(ContactFormModel model)
+        {
+            if (!ModelState.IsValid)
+                return Content("Form verileri geçersiz.");
 
+            try
+            {
+                var smtp = _config.GetSection("Smtp");
 
+                var mail = new MailMessage
+                {
+                    From = new MailAddress(smtp["UserName"]),
+                    Subject = $"[İletişim] {model.Subject}",
+                    Body = $"Ad: {model.Name}\nEmail: {model.Email}\nTelefon: {model.Phone}\n\nMesaj:\n{model.Message}",
+                    IsBodyHtml = false
+                };
 
+                mail.To.Add(smtp["ToEmail"]);
+
+                var smtpClient = new SmtpClient(smtp["Host"], int.Parse(smtp["Port"]))
+                {
+                    Credentials = new NetworkCredential(smtp["UserName"], smtp["Password"]),
+                    EnableSsl = bool.Parse(smtp["EnableSSL"])
+                };
+
+                smtpClient.Send(mail);
+
+                TempData["SuccessMessage"] = "Mesajınız başarıyla gönderildi.";
+                return RedirectToAction("İletisim");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Hata: " + ex.Message;
+                return RedirectToAction("İletisim");
+            }
+        }
     }
+
 }
---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HadımkoyAnkaraNakliyat_WEB.Models
+{
+    public class ContactFormModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
