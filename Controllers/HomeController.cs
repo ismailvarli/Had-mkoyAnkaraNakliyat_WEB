17f4c8caf73efd4396480b221242682dbf1af30a
ismail varlı <ismailvarli1907@gmail.com>
2025-07-13 02:53:30 +0300

    hadimkoyankaranakliyat_WEB_guncelleme

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using HadımkoyAnkaraNakliyat_WEB.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using System.Net;
 using System.Net.Mail;
 
@@ -14,61 +13,123 @@ namespace HadımkoyAnkaraNakliyat_WEB.Controllers
         {
             _config = config;
         }
+
         public IActionResult Index()
         {
             return View();
         }
-
+        [HttpGet]   
         public IActionResult Hakkımızda()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Hakkımızda(TeklifFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = "Form verileri geçersiz.";
+                return RedirectToAction("Hakkımızda");
+            }
+            try
+            {
+                var smtp = _config.GetSection("Smtp");
+
+                var body = $@"
+                    <b>Ad Soyad:</b> {model.AdSoyad}<br>
+                    <b>Email:</b> {model.Email}<br>
+                    <b>Telefon:</b> {model.Telefon}<br>
+                    <b>Tarih:</b> {model.Tarih}<br>
+                    <b>Ağırlık:</b> {model.Agirlik}<br>
+                    <b>Alınacak Şehir:</b> {model.AlinanSehir}<br>
+                    <b>Taşınacak Şehir:</b> {model.TasinanSehir}<br>
+                ";
+
+                var mail = new MailMessage
+                {
+                    From = new MailAddress(smtp["UserName"]),
+                    Subject = "[Hakkımızda Teklif] Yeni Talep",
+                    Body = body,
+                    IsBodyHtml = true
+                };
+
+                mail.To.Add(smtp["ToEmail"]);
+
+                var smtpClient = new SmtpClient(smtp["Host"], int.Parse(smtp["Port"]))
+                {
+                    Credentials = new NetworkCredential(smtp["UserName"], smtp["Password"]),
+                    EnableSsl = bool.Parse(smtp["EnableSSL"])
+                };
+
+                smtpClient.Send(mail);
+
+                TempData["SuccessMessage"] = "Teklif talebiniz başarıyla gönderildi.";
+                return RedirectToAction("Hakkımızda");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Hata: " + ex.Message;
+                return RedirectToAction("Hakkımızda");
+            }
         }
 
         public IActionResult Hizmetlerimiz()
         {
             return View();
         }
+
         public IActionResult Hadimköy_Ankara_Nakliyat()
         {
             return View();
         }
+
         public IActionResult İstanbul_Ankara_Nakliyat()
         {
             return View();
         }
+
         public IActionResult İstanbul_Ankara_Kamyonet_Nakliyat()
         {
             return View();
         }
+
         public IActionResult İstanbul_Ankara_Ambar_Nakliyat()
         {
             return View();
         }
+
         public IActionResult İstanbul_Ankara_Parsiyel_Nakliyat()
         {
             return View();
         }
+
         public IActionResult İstanbul_Ankara_Şehirlerarasi_Nakliyat()
         {
             return View();
         }
+
         public IActionResult İstanbul_Ankara_Evden_Eve_Nakliyat()
         {
             return View();
         }
+
         public IActionResult İstanbul_Ankara_Ceyiz_Nakliyat()
         {
             return View();
         }
+
         public IActionResult İstanbul_Ankara_Fuar_Nakliyat()
         {
             return View();
         }
+
         [HttpGet]
         public IActionResult İletisim()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult İletisim(ContactFormModel model)
         {
@@ -107,5 +168,4 @@ namespace HadımkoyAnkaraNakliyat_WEB.Controllers
             }
         }
     }
-
 }
---
+++ Models/TeklifFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HadımkoyAnkaraNakliyat_WEB.Models
+{
+    public class TeklifFormModel
+    {
+        [Required(ErrorMessage = "Ad Soyad zorunlu.")]
+        public string AdSoyad { get; set; }
+
+        [Required(ErrorMessage = "Email zorunlu.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefon zorunlu.")]
+        public string Telefon { get; set; }
+
+        public string Tarih { get; set; }
+        public string? Agirlik { get; set; }
+        public string? AlinanSehir { get; set; }
+        public string? TasinanSehir { get; set; }
+    }
+}
